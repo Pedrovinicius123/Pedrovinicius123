78f68dfa81567b5c957a20aef0c77e0725d0b819
Pedro Vinicius <120400955+Pedrovinicius123@users.noreply.github.com>
2023-01-17 13:28:01 +0000

    I added templates (html files) to the project

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('\homepage')
+def homepage():
+  return render_template('homepage.html')
+
+
+@app.route('\dev_log')
+def dev_log():
+  return render_template('devlog.html')
+
+@app.route('\contatos')
+def contatos():
+  return render_template('contatos.html')
 
 app.run(debugger=True)
