1b0bfa28a46c9ecc94302b6cca707263873b696c
Pedro Vinicius <120400955+Pedrovinicius123@users.noreply.github.com>
2023-01-17 20:30:30 +0000

    I did it! I linked the css file to the html file!

--- main.py
+++ main.py
@@ -2,28 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-try:
-  @app.route('/')
-  def homepage():
-    return render_template('homepage.html')
-    
-except Exception as err_0:
-  print(f'{err_0}')
 
-try:  
-  @app.route('/dev_log')
-  def dev_log():
-    return render_template('devlog.html')
+@app.route('/')
+def homepage():
+  return render_template('homepage.html')
 
-except Exception as err_1:
-  print(f'{err_1}')
-  
-try:
-  @app.route('/contatos')
-  def contatos():
-    return render_template('contatos.html')
-
-except Exception as err_2:
-  print(f'{err_2}')
-
-app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=8080)
