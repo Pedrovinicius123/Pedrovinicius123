eea0ff2c790b802d978e728b72d1833c7ce2b930
Pedro Vinicius <120400955+Pedrovinicius123@users.noreply.github.com>
2023-01-17 15:22:56 +0000

    I tried to add a favicon...

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 try:
-  @app.route('/homepage')
+  @app.route('/')
   def homepage():
     return render_template('homepage.html')
     
 except Exception as err_0:
   print(f'{err_0}')
 
-try:
-  
+try:  
   @app.route('/dev_log')
   def dev_log():
     return render_template('devlog.html')
 
 except Exception as err_1:
   print(f'{err_1}')
+  
 try:
   @app.route('/contatos')
   def contatos():
@@ -26,4 +26,4 @@ try:
 except Exception as err_2:
   print(f'{err_2}')
 
-app.run(debug=True)
+app.run(debug=True, host='0.0.0.0', port=8080)
